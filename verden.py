d7908422b3641b10d387a67a2b9ea5a2a9e8fae0
inevassgardeid <123069085+inevassgardeid@users.noreply.github.com>
2023-01-26 10:50:10 +0100

    verden oppdatert

--- verden.py
+++ verden.py
@@ -1,2 +1,11 @@
-def hei():
-    pass
+from politiker import Politiker
+import json
+
+
+fil = open("politiker.json")
+data = json.load(fil)
+fil.close()
+politikere = []
+
+for politiker in data["representanter_oversikt"]["representanter_liste"]["representant"]:
+    ny = Politiker(politiker["fornavn"], politiker["etternavn"], politiker["parti"]["navn"])
