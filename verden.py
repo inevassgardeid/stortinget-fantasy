1d9c3277a991451bd2af483bda4247a74db498c8
SnorreEngh <122024096+SnorreEngh@users.noreply.github.com>
2023-01-26 11:43:12 +0100

    update verden.py

--- verden.py
+++ verden.py
@@ -1,5 +1,5 @@
 import json
-
+from politiker import Politiker
 
 fil = open("politiker.json")
 data = json.load(fil)
@@ -12,3 +12,19 @@ for politiker in data["representanter_oversikt"]["representanter_liste"]["repres
     politikere.append(verden)
 
 print(politikere)
+
+class Verden: 
+    def __init__(self):
+        self.politikere = politikere
+        self.lag = []
+
+    def finn_politiker(self):
+        for etternavn in politikere:
+            if etternavn == ["etternavn"]:
+                self.lag.append(Politiker(["fornavn"], ["etternavn"], ["parti"]))
+
+    def legg_til_politiker(self):
+        pass
+
+    def legg_til_lag(self):
+        pass
